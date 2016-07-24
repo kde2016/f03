69bea1a4819334f7d4c23f5119dc531cd0fadfc9
kde2016 <2422332@mail.ru>
2016-07-24 14:10:51 +0700

    Initial Commit

---
+++ f03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  f03
+//
+//  Created by Denis Kartashev on 24.07.16.
+//  Copyright © 2016 Denis Kartashev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
